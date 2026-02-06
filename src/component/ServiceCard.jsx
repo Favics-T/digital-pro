24e3aa936eaf79bc5bc9b6c06da54c7a0d94be41
unknown <taiwokolapo533@gmail.com>
2026-02-06 12:13:46 +0100

    added section 2

---
+++ src/component/ServiceCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function ServiceCard({icon, title,desc,}) {
+  return (
+    <div className=
+    'border-b p-8  items-cente flex gap-8 border-purple-300'>
+      <div>
+            <div className=
+            'flex flex-col gap-4 justify-center items-center'>
+                <h1 className=
+                'text-2xl text-pink-600'>{icon}</h1>
+                <p 
+                className='text-lg font-bold border rounded-full border-pink-400 px-2 py-1'>01</p>
+            </div>
+
+      </div>
+
+      <div className='flex flex-col gap-4 '>
+        <h1 className='font-bold text-xl'>{title}</h1>
+        <p className='text-sm font-bold'>{desc}</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default ServiceCard
---
+++ src/component/Services.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { PiSpeakerHighFill } from "react-icons/pi";
+import ServiceCard from './ServiceCard'
+import { GiHeavyTimer } from "react-icons/gi";
+import { GiFlowerTwirl } from "react-icons/gi";
+import { MdOutlineConnectWithoutContact } from "react-icons/md";
+import Img3 from '../assets/Image/Img3.jpg'
+
+const servicesList = [
+    {icon:<PiSpeakerHighFill />, title:"EXPERTISE", desc:"Our team consists of professionals who have many years of experience in the field of online marketing. We understand all the subtieties and trens of this rapidly changing world"},
+    {icon:<GiFlowerTwirl />, title:"RANGE OF SERVICES", desc:"DigitalPro offer a full range of online advertising services so you can find everything you need in one place"},
+    {icon:<GiHeavyTimer />, title:"EFFECTIVENESS", desc:"Our goal is not just to advertise, but to achieve concrete results We work on your success and are confident in achieving your goals."},
+    {icon:<MdOutlineConnectWithoutContact />, title:"COMMUNITY OF CLIENTS", desc:"We are creating a true community of clients, where the exchange of experience and knowledge contributes to mutual growth and development"},
+]
+
+function Services() {
+  return (
+    <div
+    className='relative bg-[url("/images/Bg4.jpg")] bg-cover bg-center bg-no-repeat -mt-10'>
+      <div className=''>
+
+        <h1 className='text-7xl text-center mt-8 font-bold'>Digital <span className='
+       block bg-gradient-to-r bg-clip-text text-transparent from-indigo-800 via-pink-200 to-pink-500 '>Pro </span></h1>
+
+<div className='flex  gap-24'>
+   
+   <div className='w-4/10'>
+    <img src={Img3} alt="" />
+</div>
+    <div>
+    {
+        servicesList.map((serv)=>(
+            <ServiceCard icon={serv.icon} title={serv.title} desc={serv.desc} />
+        ))
+    }
+</div>
+
+
+
+</div>
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Services
